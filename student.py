6426c789209896c020ef120f65b1c4a18b0b10e0
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-12 12:23:52 +0000

    testing methods and properties

--- student.py
+++ student.py
@@ -1,7 +1,8 @@
 from datetime import date, timedelta
 
+
 class Student:
-    """A Student class as base for method testing"""
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
@@ -9,8 +10,7 @@ class Student:
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
-        
-        @property
-        def full_name(self):
-            return f"{self._first_name} {self._last_name}"
 
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
